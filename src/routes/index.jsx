d09299c1c795a2aabee1c3a072f06bc7caf7eec4
Pedro Luz <pedroluz895@gmail.com>
2024-08-15 13:54:11 -0300

    tela de login com autenticacao e backend

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
+import Login from "../pages/Login";
 
 
 const AppRoutes = () => {
    return(
        <BrowserRouter>
             <Routes>
-            <Route element = { <Home/> }  path="/" exact />
+            <Route element = { <Home/> }  path="/home" exact />
+            <Route element = { <Login /> }  path="/login" exact />
             </Routes>
        </BrowserRouter>
    )
